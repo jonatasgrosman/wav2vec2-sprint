27c3523a9fe6ce352287c4f750aa4af8395ae607
Jonatas Grosman <jonatasgrosman@gmail.com>
2021-03-25 14:43:23 -0300

    adding some quotation marks in chars_to_ignore list

--- run_common_voice.py
+++ run_common_voice.py
@@ -153,7 +153,7 @@ class DataTrainingArguments:
         },
     )
     chars_to_ignore: List[str] = list_field(
-        default=[",", "?", ".", "!", "-", ";", ":", '""', "%", "'", '"', "�", "·", "჻", "¿", "¡", "~", "՞", "؟", "،", "।", "॥"],
+        default=[",", "?", ".", "!", "-", ";", ":", '""', "%", "'", '"', "�", "·", "჻", "¿", "¡", "~", "՞", "؟", "،", "।", "॥", "«", "»", "„", "“", "”", "「", "」", "‘", "’", "《", "》"],
         metadata={"help": "A list of characters to remove from the transcripts."},
     )
     augmentation_factor: Optional[int] = field(
